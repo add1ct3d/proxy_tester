4574e79a07223ae940c1295332ae19d9fa02f078
Shiuh Sen Ang <shiuhsen.ang92@outlook.com>
2016-08-04 15:59:38 -0700

    improved script

--- request.py
+++ request.py
@@ -3,11 +3,12 @@ import requests, sys
 assert len(sys.argv) == 3
 
 proxy = 'http://{}:{}'.format(sys.argv[1], sys.argv[2])
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
 
 print proxy
 
 try:
-	r = requests.get('http://shiuhsenang.com', proxies = {'http': proxy}, timeout = 30)
+	r = requests.get('http://shiuhsenang.com', headers = headers, proxies = {'http': proxy}, timeout = 30)
 	print r
 except requests.exceptions.Timeout:
 	sys.exit(-1)
