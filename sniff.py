8e1b5623e5e233f12797ba41cbf5f24c24dc5013
Ubuntu <ubuntu@shiuhsenang.com>
2016-08-04 23:13:30 +0000

    improved script

--- sniff.py
+++ sniff.py
@@ -4,16 +4,17 @@ from scapy.all import *
 assert len(sys.argv) == 3
 
 def callback(p):
-	#if IP in p and TCP in p:
-	#	print p[IP].src + ':' + str(p[TCP].sport) + ' -> ' + p[IP].dst + ':' + str(p[TCP].dport)
+	#print p[IP].src + ':' + str(p[TCP].sport) + ' -> ' + p[IP].dst + ':' + str(p[TCP].dport)
 
-	if IP in p and p[IP].src == sys.argv[1]  and TCP in p and p[TCP].dport == 80 and Raw in p:
+	if  Raw in p:
 		raw = str(p[Raw])
 		
 		header = raw.split('\r\n\r\n')[0]
 
 		print header
 
-filter = 'tcp and host {} and port {}'.format(sys.argv[1], sys.argv[2])
+filter = 'tcp and src host {} and dst port 80'.format(sys.argv[1])
+
+print filter
 
 sniff(filter = filter, prn = callback, store = 0, timeout = 30)
