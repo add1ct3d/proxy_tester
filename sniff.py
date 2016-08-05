a8966f553093a724e68a0f0ff41c1f98ebde2b6a
Ubuntu <ubuntu@shiuhsenang.com>
2016-08-05 00:09:47 +0000

    improved script

--- sniff.py
+++ sniff.py
@@ -4,9 +4,9 @@ from scapy.all import *
 assert len(sys.argv) == 3
 
 def callback(p):
-	#print p[IP].src + ':' + str(p[TCP].sport) + ' -> ' + p[IP].dst + ':' + str(p[TCP].dport)
+	print p[IP].src + ':' + str(p[TCP].sport) + ' -> ' + p[IP].dst + ':' + str(p[TCP].dport)
 
-	if  Raw in p:
+	if Raw in p:
 		raw = str(p[Raw])
 		
 		header = raw.split('\r\n\r\n')[0]
@@ -21,10 +21,11 @@ def callback(p):
 			d['summary'] = 'The proxy server ' + sys.argv[1] + ':' + sys.argv[2] + ' is transparent'
 		elif 'Via' in d:
 			d['summary'] = 'The proxy server ' + sys.argv[1] + ':' + sys.argv[2] + ' is anonymous'
-		elif:
+		else:
 			d['summary'] = 'The proxy server ' + sys.argv[1] + ':' + sys.argv[2] + ' is elite anonymous'
 
-		json.dumps(d)
+		print d
+		print json.dumps(d)
 		sys.exit(0)
 
 filter = 'tcp and src host {} and dst port 80'.format(sys.argv[1])
